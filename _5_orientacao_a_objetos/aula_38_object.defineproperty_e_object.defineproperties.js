03730c59eae8f728bc2777379f3ff1751bc9fe7a
matos <matos.gustavo@outlook.com>
2024-03-21 00:42:43 -0300

    Finalização Aula 38 - Módulo 05

---
+++ _5_orientacao_a_objetos/aula_38_object.defineproperty_e_object.defineproperties.js
@@ -0,0 +1,35 @@
+
+// Object.defineProperties" => Possível declarar várias propriedades
+// Object.defineProperty" => Possível declarar apenas uma propriedade
+
+const propriedadePadrao = (valorInicial) => ({
+    value: valorInicial,
+    writable: false, // Habilita ou desabilita a edição direta com: usuario.nome = 'novo nome'
+    configurable: false, // Habilita ou desabilita deletar ou alterar com um novo defineProperty
+    enumerable: true // Habilita ou desabilita a exibição no objeto
+})
+
+class Usuario {  
+
+    constructor(nome, email, senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+
+        Object.defineProperty(this, 'nome',{
+            value: nome,
+            writable: false, // Habilita ou desabilita a edição direta com: usuario.nome = 'novo nome'
+            configurable: false, // Habilita ou desabilita deletar ou alterar com um novo defineProperty
+            enumerable: true // Habilita ou desabilita a exibição no objeto
+        })
+
+        Object.defineProperties(this, {
+            senha: propriedadePadrao(senha),
+            email: propriedadePadrao(email)
+        })
+    }
+}
+
+const usuario = new Usuario('Gustavo', 'matos@email.com', 'senha@123');
+
+console.log(usuario);
