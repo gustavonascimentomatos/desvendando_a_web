8452b24a2bd42c8551bf8e8331c9330feaf1d057
matos <matos.gustavo@outlook.com>
2024-03-21 22:13:02 -0300

    Finalização Aula 39 - Módulo 05

--- _5_orientacao_a_objetos/aula_39_get_e_set.js
+++ _5_orientacao_a_objetos/aula_39_get_e_set.js
@@ -1,14 +1,3 @@
-
-// Object.defineProperties" => Possível declarar várias propriedades
-// Object.defineProperty" => Possível declarar apenas uma propriedade
-
-const propriedadePadrao = (valorInicial) => ({
-    value: valorInicial,
-    writable: false, // Habilita ou desabilita a edição direta com: usuario.nome = 'novo nome'
-    configurable: false, // Habilita ou desabilita deletar ou alterar com um novo defineProperty
-    enumerable: true // Habilita ou desabilita a exibição no objeto
-})
-
 class Usuario {  
 
     constructor(nome, email, senha) {
@@ -21,12 +10,25 @@ class Usuario {
         })
 
         Object.defineProperties(this, {
-            senha: propriedadePadrao(senha),
-            email: propriedadePadrao(email)
+            nome:{
+                get: () => nome,
+                set: (value) => nome = value
+            },
+            senha: {
+                get: () => senha,
+                set: (value) => {
+                    if(value.length < 4){
+                        throw new TypeError('Mínimo 4 caracteres')
+                    }
+                }
+            },
+            email:{
+                get: () => email,
+                set: (value) => email = value
+            }
         })
     }
 }
 
 const usuario = new Usuario('Gustavo', 'matos@email.com', 'senha@123');
-
 console.log(usuario);
